6090d6b459cf7c68db0cd5c49861dd45a4e1896a
ojnode <ojinnakabu95@gmail.com>
2022-12-06 18:48:22 +0100

    add n numbers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
 import sys
-argv = sys.argv[1:]
-argv_count = len(argv)
-index = 1
-res = 0
-while index <= argv_count:
-res += int(sys.argv[index])
-index += 1
-print("{:d}".format(res))
+
+if __name__ == "__main__":
+
+    i = 0
+    result = 0
+
+    for arg in sys.argv:
+        if i != 0:
+            result += int(arg)
+        else:
+            i += 1
+    print("{:d}".format(result))
