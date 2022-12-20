eed578b844e6aba3a89d70a2b4906a21cc4f63e3
ojnode <ojinnakabu95@gmail.com>
2022-12-21 00:52:08 +0100

    a class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-square.py by Ojinnaka Prince
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def _init_(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square defined
+        """
+
+        self._size = size
