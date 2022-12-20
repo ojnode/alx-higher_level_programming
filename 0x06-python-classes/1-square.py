d7f6561a42ebcd2356385b4cf28c6adff94469c8
ojnode <ojinnakabu95@gmail.com>
2022-12-21 00:57:59 +0100

    defines a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,9 +6,5 @@
 class Square:
     """Represents a square"""
 
-    def _init_(self, size):
-        """Initializing this square class
-        Args: size - represents the size of the square defined
-        """
-
-        self._size = size
+    def  __init__(self, size):
+        self.__size = size
